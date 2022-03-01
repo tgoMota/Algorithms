4909576bb76ff437fc5b5e4a7b94bd5d5584605e
Thiago <tgomota1@gmail.com>
2022-02-28 22:28:27 -0300

    add kuhn algo

---
+++ kuhn.cpp
@@ -0,0 +1,24 @@
+vector<int> match;
+vector<bool> vst;
+vector<vector<int>> adj;
+int Aug(int l) {
+  if (vst[l]) return 0;
+  vst[l] = 1;
+  for (int x : adj[l]) {
+    if (match[x] == -1 || Aug(match[x])) {
+      match[x] = l; 
+      return 1;
+    } 
+  }
+  return 0;
+}
+ 
+int kuhn(const int n){
+  int ans = 0;
+  match.assign(n+1, -1);
+  for (int l = 1; l <= n ;l++) {
+    vst.assign(n+1, 0);
+    ans += Aug(l);
+  }
+  return ans;
+}
