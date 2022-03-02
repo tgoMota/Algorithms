4a82706088330cd5d69df2692e8f6d8b0ecae01b
Thiago <tgomota1@gmail.com>
2022-03-02 19:28:17 -0300

    update unionFind

--- unionFind.cpp
+++ unionFind.cpp
@@ -1,56 +1,33 @@
-//Union find with path compression
+#include <bits/stdc++.h>
+using namespace std;
+//Union find by size
 struct UF{
-  int N;
-  vector<int> p;
+  vector<int> p, sz;
+  UF(){};
   UF(const int n){
-    N = n;
-    p.resize(n+1);
+    init(n);
+  }
+  void init(const int n){
+    p.assign(n+1, -1);
+    sz.assign(n+1, 1);
     for(int i = 0; i <= n ; ++i) p[i] = i;
   }
-
-  int find(int x){
-    if(p[x] == x) return x;
-    return p[x] = find(p[x]);
+  
+  int find_set(int u){
+    return p[u] = (p[u] == u ? u : find_set(p[u]));
   }
-
-  bool isSame(int u, int v){
-    return find(u) == find(v);
-  }
-
-  void merge(int a, int b){
-    p[find(a)] = find(b);
-  }
-};
-
-
-//Union find with path compression by rank
-struct UF{
-  int N;
-  vector<int> p, rnk;
-  UF(const int n){
-    N = n;
-    p.resize(n+1);
-    rnk.resize(n+1);
-    for(int i = 0; i <= n ; ++i) p[i] = i, rnk[i] = 0;
-  }
- 
-  int find(int x){
-    if(p[x] == x) return x;
-    return find(p[x]);
-  }
- 
-  bool isSame(int u, int v){
-    return find(u) == find(v);
-  }
- 
-  void merge(int a, int b){
-    a = find(a);
-    b = find(b);
-    if(rnk[a] < rnk[b]) p[a] = b;
-    else if(rnk[a] > rnk[b]) p[b] = a;
-    else{
-      p[b] = a;
-      rnk[a]++;
-    }
+  
+  void union_set(int a, int b) {
+      a = find_set(a);
+      b = find_set(b);
+      if (a != b) {
+          if (sz[a] < sz[b]) swap(a, b);
+          p[b] = a;
+          sz[a] += sz[b];
+      }
   }
 };
+
+int main(){
+  
+}
